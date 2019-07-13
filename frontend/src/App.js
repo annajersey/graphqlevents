adc9f7d144d479ed2c14256ccfbe941edbbce1c4
anna <anna.brodskaya@gmail.com>
2019-07-13 12:34:03 +0300

    Navigation basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,23 @@ import './App.css';
 import AuthPage from "./pages/Auth";
 import Bookings from "./pages/Bookings";
 import Events from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Switch>
-                    <Redirect from="/" to="/auth" exact/>
-                    <Route path="/auth" component={AuthPage}/>
-                    <Route path="/events" component={Events}/>
-                    <Route path="/bookings" component={Bookings}/>
-                </Switch>
+                <React.Fragment>
+                <MainNavigation/>
+                <main className="main-content">
+                    <Switch>
+                        <Redirect from="/" to="/auth" exact/>
+                        <Route path="/auth" component={AuthPage}/>
+                        <Route path="/events" component={Events}/>
+                        <Route path="/bookings" component={Bookings}/>
+                    </Switch>
+                </main>
+                </React.Fragment>
             </BrowserRouter>
         </div>
     );
